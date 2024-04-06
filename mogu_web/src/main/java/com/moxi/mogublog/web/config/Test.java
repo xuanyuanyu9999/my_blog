1906baf6d1e9f51400e8ef29085401f34a44502f
yu-xuanyuan <xuan.yy@asiainfo-sec.com>
2024-04-06 19:37:37 +0800

    my blog commit

---
+++ mogu_web/src/main/java/com/moxi/mogublog/web/config/Test.java
@@ -0,0 +1,21 @@
+package com.moxi.mogublog.web.config;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author yu_xuanyuan
+ * @date 2023/5/9 15:27
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+        
+
+    }
+    public static class A{
+        private String name;
+        private Integer age;
+    }
+}
---
+++ uniapp_mogu/Readme.md
@@ -0,0 +1,14 @@
+## 首页截图
+
+<p style="text-align: center;"><img src="https://s3.bmp.ovh/imgs/2022/02/d0776b3bd128eaa7.png" alt="仿稀土掘金蘑菇博客截图"></img></p>
+
+## 前言
+项目借鉴稀土掘金美化页面，更规范的使用组件提升性能，优化了操作逻辑、增强了用户体验，引用uview框架与particles粒子动效插件，有Bug提交请移步到issues。	  
+ 
+## 开始使用
+
+1.拉取代码
+
+2.在HBuilderX中导入代码
+
+3.选择运行到浏览器或模拟器
